1d040531dfccbe53ce3695084f543f30f55962c3
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-26 10:39:33 +0300

    Updates

---
+++ precache-manifest.983296b52987f26bf2ce2d0fd03a48bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97fd8c05357b58545be4ff5456dc4a1",
+    "url": "/github.io/cursorHW_19/index.html"
+  },
+  {
+    "revision": "11a968f567a29defed97",
+    "url": "/github.io/cursorHW_19/static/css/main.5ae6aaa4.chunk.css"
+  },
+  {
+    "revision": "a0ae5ae1b527f8bf5cd0",
+    "url": "/github.io/cursorHW_19/static/js/2.cdcfb89a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/cursorHW_19/static/js/2.cdcfb89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a968f567a29defed97",
+    "url": "/github.io/cursorHW_19/static/js/main.3ba25c3d.chunk.js"
+  },
+  {
+    "revision": "8cac9e66732e035aa166",
+    "url": "/github.io/cursorHW_19/static/js/runtime-main.b20441cf.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/github.io/cursorHW_19/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
